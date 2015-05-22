3223ba5096d3721bb2854917520bceb3ed792f62
caye <kevin-ca@club-internet.fr>
2015-05-22 17:30:35 +0200

    c'est vendredi bitch

--- dataExploration.py
+++ dataExploration.py
@@ -1,10 +1,36 @@
 import pandas as pd
 import data_utils as du
-train = pd.read_csv("data/train.csv" )
+train = pd.read_csv("Data/train.csv" )
 
-import pygmaps 
+import visualization as vi
 
-mymap = pygmaps.maps( 41.141412, -8.618643, 16)
-mymap.addpath( du.stringpolyline_to_listpolyline(train.POLYLINE[0]) )
-mymap.addpath( du.stringpolyline_to_listpolyline(train.POLYLINE[1]) )
+
+mymap = vi.pygmaps.maps( 41.141412, -8.618643, 16)
+polylines = map( du.stringpolyline_to_listpolyline, train.POLYLINE[0:100] )
+vi.plot_startpoints( polylines, mymap  )
+vi.plot_endpoints( polylines, mymap  )
+vi.plot_path( polylines, mymap  )
+mymap.draw('mymap.html')
+
+
+#Visualize test set
+test = pd.read_csv("Data/test.csv" )
+mymap = vi.pygmaps.maps( 41.141412, -8.618643, 16)
+polylines = map( du.stringpolyline_to_listpolyline, test.POLYLINE[1:100] )
+vi.plot_startpoints( polylines, mymap  )
+vi.plot_path( polylines, mymap  )
+mymap.draw('mymap.html')
+
+#Visualize bounding box
+polylines = map( du.stringpolyline_to_listpolyline, train.POLYLINE )
+minlat = min( map( du.min_latitute, polylines ) )
+maxlat = max( map( du.max_latitute, polylines ) )
+
+minlong = min( map( du.min_longitude, polylines ) )
+maxlong = max( map( du.max_longitude, polylines ) )
+mymap = vi.pygmaps.maps( 41.141412, -8.618643, 16)
+mymap.addpoint( minlat, minlong, "#FF0000")
+mymap.addpoint( minlat, maxlong, "#FF0000")
+mymap.addpoint( maxlat, minlong, "#FF0000")
+mymap.addpoint( maxlat, maxlong, "#FF0000")
 mymap.draw('mymap.html')
--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,23 @@
 import numpy as np
+import pandas as pd
 
-def stringpolyline_to_arraypolyline( stringpolyline ): 
-    return np.array( map( lambda l : [ float(a) for a in l ], \
-    [ a.split(',') for a in stringpolyline[2:-2].split('],[') ] ) )
     
-    
-def stringpolyline_to_listpolyline( stringpolyline ): 
+def stringpolyline_to_listpolyline( stringpolyline ):
+    if stringpolyline == '[]':
+        return []
     return map( lambda l : ( float(l[1]), float(l[0])), \
     [ a.split(',') for a in stringpolyline[2:-2].split('],[') ] )
     
     
+def min_latitute( listpolyline ):
+    return min( listpolyline, key = lambda x : x[0] )[0]
+    
+def max_latitute( listpolyline ):
+    return max( listpolyline, key = lambda x : x[0] )[0]
+    
+def max_longitude( listpolyline ):
+    return max( listpolyline, key = lambda x : x[1] )[1]
+    
+def min_longitude( listpolyline ):
+    return min( listpolyline, key = lambda x : x[1] )[1]
+
